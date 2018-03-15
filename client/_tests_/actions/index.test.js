c4613dae78f194418458dad2da396797e0eccac1
Bit <reijonen.miika@gmail.com>
2018-03-15 23:11:40 +0200

    Added action and reducer tests

--- client/_tests_/actions/index.test.js
+++ client/_tests_/actions/index.test.js
@@ -1,4 +1,4 @@
-import * as actions from '../../actions/index';
+import * as actions from '../../src/actions/index';
 
 describe('actions', () => {
   it('should create an action to add new cart item', () => {
@@ -7,6 +7,30 @@ describe('actions', () => {
       type: "CARTITEM_SELECTED",
       payload: item
     }
-    expect(actions.selectCartItem(item).toEqual(expectedAction))
+    expect(actions.selectCartItem(item)).toEqual(expectedAction)
+  })
+  it('should create an action to remove cart item', () => {
+    const item = {name: 'Brown jacket'};
+    const expectedAction = {
+      type: "REMOVE_CARTITEM",
+      payload: item.name
+    }
+    expect(actions.removeCartItem(item)).toEqual(expectedAction)
+  })
+  it('should create an action to increase quantity of cart item', () => {
+    const item = {name: 'Brown jacket', qty: 1};
+    const expectedAction = {
+      type: "INCRESE_QTY",
+      payload: item
+    }
+    expect(actions.increaseQty(item)).toEqual(expectedAction)
+  })
+  it('should create an action to decrease quantity of cart item', () => {
+    const item = {name: 'Brown jacket', qty: 2};
+    const expectedAction = {
+      type: "DECRESE_QTY",
+      payload: item
+    }
+    expect(actions.decreaseQty(item)).toEqual(expectedAction)
   })
 })
--- client/_tests_/reducers/reducer-newCartItem.test.js
+++ client/_tests_/reducers/reducer-newCartItem.test.js
@@ -1,7 +1,29 @@
+import reducer from '../../src/reducers/reducer-newCartItem';
 
-
-describe('Request Reducer', () => {
-  it('has a default state', () => {
-    expect(Array.isArray([])).toBe(true);
-  });
-});
+describe('newCartItem reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual([])
+  })
+  it('should handle CARTITEM_SELECTED', () => {
+    expect(
+      reducer([], {
+        type: "CARTITEM_SELECTED",
+        payload: {name: "product", price: "20"}
+      })
+    ).toEqual([
+      {
+        name: 'product',
+        price: "20",
+        qty: 1
+      }
+    ])
+  })
+  it('should handle REMOVE_CARTITEM', () => {
+    expect(
+      reducer([], {
+        type: "REMOVE_CARTITEM",
+        payload: {name: "product"}
+      })
+    ).toEqual([])
+  })
+})
